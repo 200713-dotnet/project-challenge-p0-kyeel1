9a7ff4f19b28aaa5a18ee013bf15a99bdc03c2c8
henry <kyeel123@gmail.com>
2020-07-29 12:58:41 -0400

    i dont know what changed

--- PizzaStore/PizzaStore.Storing/PIZZASTORE2Context.cs
+++ PizzaStore/PizzaStore.Storing/PIZZASTORE2Context.cs
@@ -30,7 +30,6 @@ namespace PizzaStore.Storing
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseSqlServer("server=localhost;database=PIZZASTORE2;user id=sa;password=Dragoon123");
             }
         }
